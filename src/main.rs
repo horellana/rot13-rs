1402758baef2340dc95aeef8897977b29eb3e228
Hector Orellana <hector@zooft.company>
2022-07-13 21:58:40 -0400

    solve https://www.codewars.com/kata/530e15517bc88ac656000716/train/rust

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,38 @@
-use std::io;
 use std::process;
 
-const ASCII_TABLE_LENGTH: usize = 127;
-const ASCII_TABLE: [u8; ASCII_TABLE_LENGTH] = generate_cipher_table();
+const fn get_lowercase_cipher_index(index: u8) -> u8 {
+    let mut character_index: u8 = index;
+    let mut count = 0;
+
+    while count < 13 {
+        character_index = character_index + 1;
+
+        if character_index > 122 {
+            character_index = 97;
+        }
+
+        count = count + 1;
+    }
+
+    character_index
+}
+
+const fn get_uppercase_cipher_index(index: u8) -> u8 {
+    let mut character_index: u8 = index;
+    let mut count = 0;
+
+    while count < 13 {
+        character_index = character_index + 1;
+
+        if character_index > 90 {
+            character_index = 65;
+        }
+
+        count = count + 1;
+    }
+
+    character_index
+}
 
 const fn get_cipher_index(index: u8) -> u8 {
     let mut character_index: u8 = index;
@@ -23,14 +53,53 @@ const fn get_cipher_index(index: u8) -> u8 {
 
 const fn generate_cipher_table() -> [u8; ASCII_TABLE_LENGTH]  {
     let mut table: [u8; ASCII_TABLE_LENGTH] = [0; ASCII_TABLE_LENGTH];
-    let mut i = 0;
 
-    while i < ASCII_TABLE_LENGTH {
-        table[i] = get_cipher_index(i as u8);
+    let mut start: usize = 0;
+    let mut end: usize = 127;
+    let mut i: usize = start;
+
+    while i < end {
+        table[i] = i as u8;
+        i = i + 1;
+    }
+
+    start = 65;
+    end = 90;
+    i = start;
+
+    while i <= end {
+        table[i] = get_uppercase_cipher_index(i as u8);
+        i = i + 1;
+    }
+
+    start = 97;
+    end = 122;
+    i = start;
+
+    while i <= end {
+        table[i] = get_lowercase_cipher_index(i as u8);
         i = i + 1;
     }
 
     table
+}
+
+const ASCII_TABLE_LENGTH: usize = 127;
+const ASCII_TABLE: [u8; ASCII_TABLE_LENGTH] = generate_cipher_table();
+
+fn rot13(message: &str) -> String {
+    String::from(message
+                 .chars()
+                 .map(|character| {
+                     if (character as usize) < 127 {
+                         ASCII_TABLE[character as usize] as char
+                     }
+                     else {
+                         character
+                     }
+
+                 })
+                 .collect::<String>())
 }
 
 fn main() {
@@ -38,7 +107,7 @@ fn main() {
         match line_result {
             Ok(line) => {
                 for character in line.chars() {
-                    if (character as usize) > 127 || (character as usize) < 0 {
+                    if ((character as i8) > 127) || ((character as i8) < 0) {
                         eprintln!("Invalid character: {}", character);
                         process::exit(1);
                     }
@@ -56,15 +125,23 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    use crate::{get_cipher_index};
+    use crate::{rot13, get_cipher_index};
 
     #[test]
     fn test_cipher_character() {
         assert_eq!(110, get_cipher_index(97));
+        assert_eq!(rot13("z"), "m");
     }
 
     #[test]
     fn test_handle_overflow() {
         assert_eq!(7, get_cipher_index(122));
+    }
+
+    #[test]
+    fn test_rot13() {
+        assert_eq!(rot13("test"), "grfg");
+        assert_eq!(rot13("Test"), "Grfg");
+        assert_eq!(rot13("abcdefghijklmnopqrstuvwxyz"), "nopqrstuvwxyzabcdefghijklm");
     }
 }
